5663f40bdda7c1d06b7b7aa0d442bc0ae95b5af8
deanhennessy <deanhennessy321@gmail.com>
2026-09-14 14:48:16 +0000

    Complete lab01

---
+++ lab01/hello.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+
+course = input("What course are you studying?")
+
+area = input("!What is your favourite area of cybersecurity?")
+
+print("Hello, " + name + "!")
+print("You are studying " + course +"" )
+print("Your favourite area in Cybersecurity is" + area + "" )
---
+++ lab01/student.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+num = input("What is your student number? ")
+year = input("What year are you in? ")
+
+print("Name:" + name + "")
+print("Student Number" + num + "")
+print("Year" + year + "")
+
